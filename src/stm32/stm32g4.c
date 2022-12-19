1c1ddf492753fdcd82ae692b82ee4eb9bd7251fa
ben5459 <104525636+ben5459@users.noreply.github.com>
2022-12-19 13:45:19 -0600

    Update stm32g4.c

--- src/stm32/stm32g4.c
+++ src/stm32/stm32g4.c
@@ -51,37 +51,6 @@ lookup_clock_line(uint32_t periph_base)
                               .rst = &RCC->AHB2RSTR,
                               .bit = 1 << pos};
     }
-    if ((periph_base == FDCAN1_BASE) || (periph_base == FDCAN2_BASE))
-        return (struct cline){.en=&RCC->APBENR1,.rst=&RCC->APBRSTR1,.bit=1<<12};
-    if (periph_base == USB_BASE)
-        return (struct cline){.en=&RCC->APBENR1,.rst=&RCC->APBRSTR1,.bit=1<<13};
-    if (periph_base == CRS_BASE)
-        return (struct cline){.en=&RCC->APBENR1,.rst=&RCC->APBRSTR1,.bit=1<<16};
-    if (periph_base == I2C3_BASE)
-        return (struct cline){.en=&RCC->APBENR1,.rst=&RCC->APBRSTR1,.bit=1<<23};
-    if (periph_base == TIM1_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<11};
-    if (periph_base == SPI1_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<12};
-    if (periph_base == USART1_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<14};
-    if (periph_base == TIM14_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<15};
-    if (periph_base == TIM15_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<16};
-    if (periph_base == TIM16_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<17};
-    if (periph_base == TIM17_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<18};
-    if (periph_base == ADC1_BASE)
-        return (struct cline){.en=&RCC->APBENR2,.rst=&RCC->APBRSTR2,.bit=1<<20};
-    if (periph_base >= APBPERIPH_BASE && periph_base <= LPTIM1_BASE)
-    {
-        uint32_t bit = 1 << ((periph_base - APBPERIPH_BASE) / 0x400);
-        return (struct cline){.en=&RCC->APBENR1, .rst=&RCC->APBRSTR1, .bit=bit};
-    }
-    // unknown peripheral. returning .bit=0 makes this a no-op
-    return (struct cline){.en=&RCC->APBENR1, .rst=NULL, .bit=0};
 }
 
 // Return the frequency of the given peripheral clock
@@ -187,8 +156,6 @@ clock_setup(void)
         ;
 }
 
-
-
 /****************************************************************
  * Bootloader
  ****************************************************************/
@@ -200,21 +167,7 @@ clock_setup(void)
 void
 bootloader_request(void)
 {
-    try_request_canboot();
-    usb_reboot_for_dfu_bootloader();
-}
-
-// Reboot into USB "HID" bootloader
-static void
-usb_hid_bootloader(void)
-{
-    irq_disable();
-    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-    RCC->APB1ENR;
-    PWR->CR |= PWR_CR_DBP;
-    RTC->BKP4R = 0x424C; // HID Bootloader magic key
-    PWR->CR &= ~PWR_CR_DBP;
-    NVIC_SystemReset();
+    dfu_reboot();
 }
 
 // Flag that bootloader is desired and reboot
